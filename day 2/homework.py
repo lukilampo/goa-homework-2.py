2052dd177ed6dd6545adbdb9f8cb5c0731187b5e
lukilampo <158604576+lukilampo@users.noreply.github.com>
2024-02-08 18:01:52 +0300

    if you want to open this file you have to open it with a code

---
+++ day 2/homework.py
@@ -0,0 +1,5 @@
+print("I subscribed to low budget stories and liked some of their videos. All in all it is a great Channel and i recommend you watching it")
+
+print("well i have not met anybody yet in goa or are friends with him/her but in the future im paling to met someone who likes programming just as much as i do ")
+
+print("I think goa is a perfect place for programmers and not only that in goa there is a more comfortableand relaxing atmosphere than other programming studios")
